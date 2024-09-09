9718ff7a9955f7ee0b448e04c0b00667d4202ccd
shental17 <133120470+shental17@users.noreply.github.com>
2024-09-09 15:10:58 +0800

    Added Login Validation

--- week_5/caseStudy05/src/App.jsx
+++ week_5/caseStudy05/src/App.jsx
@@ -2,185 +2,35 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 // import FormValidationExample from "./FormValidationExample";
+import SignUpValidation from "./SignUpValidation";
+import LoginValidation from "./LoginValidation";
 import "./App.css";
 
 function App() {
-  const [formData, setFormData] = useState({
-    username: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-  });
+  const [isLoginPage, setIsLoginPage] = useState(true);
 
-  //setting up state variable errors to manage form validation errors
-  const [errors, setErrors] = useState({});
-
-  //setting up formStatus to manage form validation errors
-  const [formStatus, setFormStatus] = useState({
-    emailValid: false,
-    passwordsMatch: false,
-    passwordValid: false,
-    isSubmitButtonEnabled: false,
-  });
-
-  // Validate password
-  const checkPassword = (password) => {
-    const hasNumber = /[0-9]/.test(password);
-    const hasLowercase = /[a-z]/.test(password);
-    const hasUppercase = /[A-Z]/.test(password);
-    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
-    const hasLength = password.length >= 8;
-    return (
-      hasNumber && hasLowercase && hasUppercase && hasSpecialChar && hasLength
-    );
-  };
-  // Validation logic moved to a separate function
-  const validateForm = (updatedData) => {
-    // Check if email has .com
-    const emailValid = /\S+@\S+\.\S+/.test(updatedData.email);
-    // Check if passwords match after state update
-    const passwordsMatch = updatedData.password === updatedData.confirmPassword;
-    //Check if password is at least 6 characters
-    const passwordValid = checkPassword(updatedData.password);
-    // Check if all fields are filled after state update
-    const allFieldsFilled = Object.values(updatedData).every((value) =>
-      Boolean(value.trim())
-    );
-
-    setErrors((prevErrors) => ({
-      ...prevErrors,
-      username: updatedData.username ? null : "Name is required",
-      email: updatedData.email
-        ? !emailValid
-          ? "Email is not valid"
-          : null
-        : "Email is required",
-      password: updatedData.password
-        ? !passwordValid
-          ? "Password must contain 8 or more characters that are of at least one number, one special, uppercase and lowercase letter."
-          : null
-        : "Password is required",
-      confirmPassword: !passwordsMatch ? "Passwords do not match" : "",
-    }));
-
-    setFormStatus({
-      emailValid,
-      passwordsMatch,
-      passwordValid,
-      isSubmitButtonEnabled:
-        allFieldsFilled && passwordsMatch && passwordValid && emailValid,
-    });
-  };
-
-  //handleChange function is used to update the formData state based on the user input in the form fields
-  const handleChange = (event) => {
-    //extract the name (input fields) and value (user input) from the event target
-    const { name, value } = event.target;
-    setFormData((prevData) => {
-      const updatedData = {
-        //use ... spread operator to optain the existing data in formData and update the specific input field with the new value
-        ...prevData,
-        [name]: value.trimStart(),
-      };
-      validateForm(updatedData);
-      return updatedData;
-    });
-  };
-
-  //handleSubmit function is used to validate the form data when the form is submitted
-  const handleSubmit = (event) => {
-    //Prevent the default form submission behaviour
-    //Check each form field for validation errors based on the defined criteria
-    event.preventDefault();
-    setFormData((prevData) => {
-      const updatedData = {
-        //use ... spread operator to optain the existing data in formData and update the specific input field with the new value
-        ...prevData,
-      };
-      // Iterate through formData and trim each value
-      for (let key in formData) {
-        if (formData.hasOwnProperty(key)) {
-          formData[key] = formData[key].trim();
-        }
-      }
-      return updatedData;
-    });
-    alert("Form has been submitted successfully!");
-    console.log(formData);
-
-    // Reset the formData state to clear the form fields
-    setFormData({
-      username: "",
-      email: "",
-      password: "",
-      confirmPassword: "",
-    });
-    setFormStatus({
-      passwordsMatch: false,
-      passwordValid: false,
-      isSubmitButtonEnabled: false,
-    });
-    // Clear the input fields
-    event.target.reset();
+  const togglePage = () => {
+    setIsLoginPage((prevIsLoginPage) => !prevIsLoginPage);
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <h2>Sign Up</h2>
-        <label>Username*</label>
-        <input
-          name="username"
-          type="text"
-          placeholder="username"
-          className={errors.username ? "errors" : null}
-          value={formData.username}
-          onChange={handleChange}
-          required={true}
-        />
-        {errors.username && <p>{errors.username}</p>}
-        <label>Email*</label>
-        <input
-          name="email"
-          type="email"
-          placeholder="example@gmail.com"
-          className={errors.email ? "errors" : null}
-          value={formData.email}
-          onChange={handleChange}
-          required={true}
-        />
-        {errors.email && <p>{errors.email}</p>}
-        <label>Password*</label>
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          className={errors.password ? "errors" : null}
-          value={formData.password}
-          onChange={handleChange}
-          required={true}
-        />
-        {errors.password && <p>{errors.password}</p>}
-        <label>Confirm Password</label>
-        <input
-          name="confirmPassword"
-          type="password"
-          placeholder="Confirm Password"
-          className={errors.confirmPassword ? "errors" : null}
-          value={formData.confirmPassword}
-          onChange={handleChange}
-          required={true}
-        />
-        {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
-        <button
-          type="submit"
-          disabled={!formStatus.isSubmitButtonEnabled}
-          className={formStatus.isSubmitButtonEnabled ? "enabled" : "disabled"}
-        >
-          Submit
-        </button>
-      </form>
-    </>
+    <div className="page">
+      {isLoginPage ? (
+        <>
+          <LoginValidation />
+          <span>
+            Don't have an account? <a onClick={togglePage}>Sign Up</a>
+          </span>
+        </>
+      ) : (
+        <>
+          <SignUpValidation />
+          <span>
+            Already have an account? <a onClick={togglePage}>Login</a>
+          </span>
+        </>
+      )}
+    </div>
   );
 }
 
