341a9e5a9c43f5df5a0e52df806ef03ed939e6a3
shental17 <133120470+shental17@users.noreply.github.com>
2024-09-23 14:45:31 +0800

    Added Radio Button

--- week_6/CaseStudy_04/js/menuUpdate.js
+++ week_6/CaseStudy_04/js/menuUpdate.js
@@ -1,40 +1,121 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const jjQty = document.getElementsByName("justJavaQty")[0];
+//   const lsQty = document.getElementsByName("laitSingleQty")[0];
+//   const ldQty = document.getElementsByName("laitDoubleQty")[0];
+//   const csQty = document.getElementsByName("cappucinoSingleQty")[0];
+//   const cdQty = document.getElementsByName("cappucinoDoubleQty")[0];
+//   const totalPriceText = document.getElementById("totalPrice");
+//   const jjPriceText = document.getElementById("totalJustJavaPrice");
+//   const lsPriceText = document.getElementById("totalLaitPrice");
+//   const csPriceText = document.getElementById("totalCappucinoPrice");
+
+//   function calculateJustJavaPrice(jjQtyValue) {
+//     const jjPrice = 2;
+//     return jjQtyValue * jjPrice;
+//   }
+
+//   function calculateLaitPrice(lsQtyValue, ldQtyValue) {
+//     const lsPrice = 2;
+//     const ldPrice = 3;
+//     return lsQtyValue * lsPrice + ldQtyValue * ldPrice;
+//   }
+
+//   function calculateCappucinoPrice(csQtyValue, cdQtyValue) {
+//     const csPrice = 4.75;
+//     const cdPrice = 5.75;
+//     return csQtyValue * csPrice + cdQtyValue * cdPrice;
+//   }
+
+//   function calculateTotalPrice() {
+//     const jjQtyValue = parseFloat(jjQty.value) || 0;
+//     const lsQtyValue = parseFloat(lsQty.value) || 0;
+//     const ldQtyValue = parseFloat(ldQty.value) || 0;
+//     const csQtyValue = parseFloat(csQty.value) || 0;
+//     const cdQtyValue = parseFloat(cdQty.value) || 0;
+
+//     const jjPrice = calculateJustJavaPrice(jjQtyValue);
+//     const lsPrice = calculateLaitPrice(lsQtyValue, ldQtyValue);
+//     const csPrice = calculateCappucinoPrice(csQtyValue, cdQtyValue);
+//     const totalPrice = jjPrice + lsPrice + csPrice;
+
+//     updateTextFields(jjPrice, lsPrice, csPrice, totalPrice);
+//   }
+
+//   function updateTextFields(jjPrice, lsPrice, csPrice, totalPrice) {
+//     jjPriceText.value = `$${jjPrice.toFixed(2)}`;
+//     lsPriceText.value = `$${lsPrice.toFixed(2)}`;
+//     csPriceText.value = `$${csPrice.toFixed(2)}`;
+//     totalPriceText.value = `$${totalPrice.toFixed(2)}`;
+//   }
+
+//   // Add event listeners to all input fields
+//   jjQty.addEventListener("input", calculateTotalPrice);
+//   lsQty.addEventListener("input", calculateTotalPrice);
+//   ldQty.addEventListener("input", calculateTotalPrice);
+//   csQty.addEventListener("input", calculateTotalPrice);
+//   cdQty.addEventListener("input", calculateTotalPrice);
+
+//   // Initial calculation
+//   calculateTotalPrice();
+// });
+
+// Updated JavaScript
 document.addEventListener("DOMContentLoaded", function () {
-  var justJavaQty = document.getElementsByName("justJavaqty")[0];
-  var laitSingleQty = document.getElementsByName("laitSingleqty")[0];
-  var laitDoubleQty = document.getElementsByName("laitDoubleqty")[0];
-  var cappucinoSingleQty = document.getElementsByName("cappucinoSingleQty")[0];
-  var cappucinoDoubleQty = document.getElementsByName("cappucinoDoubleQty")[0];
-  var totalPriceText = document.getElementById("totalPrice");
+  const jjQty = document.getElementsByName("justJavaQty")[0];
+  const lsShot = document.getElementsByName("laitShot");
+  const lsQty = document.getElementsByName("laitQty")[0];
+  const csShot = document.getElementsByName("cappucinoShot");
+  const csQty = document.getElementsByName("cappucinoQty")[0];
+  const totalPriceText = document.getElementById("totalPrice");
+  const jjPriceText = document.getElementById("totalJustJavaPrice");
+  const lsPriceText = document.getElementById("totalLaitPrice");
+  const csPriceText = document.getElementById("totalCappucinoPrice");
+
+  function calculateJustJavaPrice(jjQtyValue) {
+    const jjPrice = 2;
+    return jjQtyValue * jjPrice;
+  }
+
+  function calculateLaitPrice(lsShotValue, lsQtyValue) {
+    const lsPrice = lsShotValue === "single" ? 2 : 3;
+    return lsQtyValue * lsPrice;
+  }
+
+  function calculateCappucinoPrice(csShotValue, csQtyValue) {
+    const csPrice = csShotValue === "single" ? 4.75 : 5.75;
+    return csQtyValue * csPrice;
+  }
 
   function calculateTotalPrice() {
-    const justJavaPrice = 2;
-    const laitSinglePrice = 2;
-    const laitDoublePrice = 3;
-    const cappucinoSinglePrice = 4.75;
-    const cappucinoDoublePrice = 5.75;
+    const jjQtyValue = parseFloat(jjQty.value) || 0;
+    const lsShotValue = lsShot[0].checked ? "single" : "double";
+    const lsQtyValue = parseFloat(lsQty.value) || 0;
+    const csShotValue = csShot[0].checked ? "single" : "double";
+    const csQtyValue = parseFloat(csQty.value) || 0;
 
-    const justJavaQtyValue = parseFloat(justJavaQty.value) || 0;
-    const laitSingleQtyValue = parseFloat(laitSingleQty.value) || 0;
-    const laitDoubleQtyValue = parseFloat(laitDoubleQty.value) || 0;
-    const cappucinoSingleQtyValue = parseFloat(cappucinoSingleQty.value) || 0;
-    const cappucinoDoubleQtyValue = parseFloat(cappucinoDoubleQty.value) || 0;
+    const jjPrice = calculateJustJavaPrice(jjQtyValue);
+    const lsPrice = calculateLaitPrice(lsShotValue, lsQtyValue);
+    const csPrice = calculateCappucinoPrice(csShotValue, csQtyValue);
+    const totalPrice = jjPrice + lsPrice + csPrice;
 
-    const totalPrice =
-      justJavaQtyValue * justJavaPrice +
-      laitSingleQtyValue * laitSinglePrice +
-      laitDoubleQtyValue * laitDoublePrice +
-      cappucinoSingleQtyValue * cappucinoSinglePrice +
-      cappucinoDoubleQtyValue * cappucinoDoublePrice;
+    updateTextFields(jjPrice, lsPrice, csPrice, totalPrice);
+  }
 
+  function updateTextFields(jjPrice, lsPrice, csPrice, totalPrice) {
+    jjPriceText.value = `$${jjPrice.toFixed(2)}`;
+    lsPriceText.value = `$${lsPrice.toFixed(2)}`;
+    csPriceText.value = `$${csPrice.toFixed(2)}`;
     totalPriceText.value = `$${totalPrice.toFixed(2)}`;
   }
 
   // Add event listeners to all input fields
-  justJavaQty.addEventListener("input", calculateTotalPrice);
-  laitSingleQty.addEventListener("input", calculateTotalPrice);
-  laitDoubleQty.addEventListener("input", calculateTotalPrice);
-  cappucinoSingleQty.addEventListener("input", calculateTotalPrice);
-  cappucinoDoubleQty.addEventListener("input", calculateTotalPrice);
+  jjQty.addEventListener("input", calculateTotalPrice);
+  lsShot[0].addEventListener("change", calculateTotalPrice);
+  lsShot[1].addEventListener("change", calculateTotalPrice);
+  lsQty.addEventListener("input", calculateTotalPrice);
+  csShot[0].addEventListener("change", calculateTotalPrice);
+  csShot[1].addEventListener("change", calculateTotalPrice);
+  csQty.addEventListener("input", calculateTotalPrice);
 
   // Initial calculation
   calculateTotalPrice();
